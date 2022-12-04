b2b14b93042f857a0c8d1897490748317dd8fb90
BDDaniel <100427351+BDDaniel@users.noreply.github.com>
2022-12-03 19:55:10 -0500

    Actualización de dependencias y librerías y desarrollo del componente "Inicio"

--- front/src/components/areas/Areas.jsx
+++ front/src/components/areas/Areas.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Areas = () => {
   return (
-    <div>Areas</div>
+    <div>
+      
+    </div>
   )
 }
 
--- front/src/components/inicio/Inicio.jsx
+++ front/src/components/inicio/Inicio.jsx
@@ -5,7 +5,14 @@ import './Inicio.css'
 const Inicio = () => {
   return (
     <div className='inicio'>
-      <h1> Proyecto Final </h1>
+      <h1 className='pt-4 paddingB8 fw-semibold'> Proyecto Final </h1>
+      <h3 className='paddingB8 fw-semibold'> Daniel Alfonso Benavides Diaz </h3>
+      <h3 className='pb-2 fw-semibold'> Roberto Morales Ortega </h3>
+      <h3 className='paddingB8 fw-semibold'> DESARROLLO WEB II - PRESENCIAL-36711 </h3>
+      <h3 className='pb-2 fw-semibold'> Programa de Ingeniería de Sistemas </h3>
+      <h3 className='pb-2 fw-semibold'> Departamento de Ciencias de la Computación y Electrónica </h3>
+      <h3 className='pb-2 fw-semibold'> Corporación Universitaria de la Costa – Barranquilla-Atlántico </h3>
+      <h3 className='fw-semibold'> 03/12/2022 </h3>
     </div>
   )
 }
